798718b5c776b2ba75b993cd4a0504f558e1ea60
Jane Venter <janeanneventer@gmail.com>
2020-08-30 06:07:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-function depthFirstSearch(rootNode, vertices, edges){
-  let exploredVertices = [];
-  let currentVertices = [rootNode];
-  let edgesWithAdjacents = edges.filter( (edge) => {
-    return edge[0] === rootNode.name || edge[1] === rootNode.name
-  })
-  let v = currentVertices.shift();
-  v.discovered = true
-  exploredVertices.push(v)
-  let edgeNames = edgesWithAdjacents.map(function(edge))
-  let adjacentVertices = vertices.filter( (vertix) => {
-    edgesWithAdjacents.map( (edge) => {
-      return vertix.name === edge[0] || vertix.name === edge[1]
-    })
-  })
+// function depthFirstSearch(rootNode, vertices, edges){
+//   let exploredVertices = [];
+//   let currentVertices = [rootNode];
+//   let edgesWithAdjacents = edges.filter( (edge) => {
+//     return edge[0] === rootNode.name || edge[1] === rootNode.name
+//   })
+//   let v = currentVertices.shift();
+//   v.discovered = true
+//   exploredVertices.push(v)
+//   let edgeNames = edgesWithAdjacents.map(function(edge))
+//   let adjacentVertices = vertices.filter( (vertix) => {
+//     edgesWithAdjacents.map( (edge) => {
+//       return vertix.name === edge[0] || vertix.name === edge[1]
+//     })
+//   })
   
-  let filteredAdjacentVertices = adjacentVertices.filter( (vertix) => {
-    return vertix.discovered === 'null'
-  })
+//   let filteredAdjacentVertices = adjacentVertices.filter( (vertix) => {
+//     return vertix.discovered === 'null'
+//   })
   
-  return filteredAdjacentVertices
-}
+//   return filteredAdjacentVertices
+// }
 
-function depthFirstSearch(rootNode,)
+function depthFirstSearch(rootNode, vertices, edges){
+  
+}
